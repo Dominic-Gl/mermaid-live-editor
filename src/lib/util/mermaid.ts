210606e4dcc3823ea6eb6598cb631b614955e79c
Dominic Gluba <gluba@aunovis.de>
2025-01-22 13:13:07 +0100

    icons added

--- src/lib/util/mermaid.ts
+++ src/lib/util/mermaid.ts
@@ -6,6 +6,49 @@ import mermaid from 'mermaid';
 mermaid.registerLayoutLoaders(elkLayouts);
 const init = mermaid.registerExternalDiagrams([zenuml]);
 
+mermaid.registerIconPacks([
+  {
+    name: 'logos',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/logos/icons.json').then((res) => res.json()),
+  },
+  {
+    name: 'cbi',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/cbi/icons.json').then((res) => res.json()),
+  },
+  {
+    name: 'simple',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/simple-icons/icons.json').then((res) => res.json()),
+  },
+  {
+    name: 'la',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/la/icons.json').then((res) => res.json()),
+  },
+  {
+    name: 'mas',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/material-symbols/icons.json').then((res) => res.json()),
+  },
+  {
+    name: 'mdi',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/mdi/icons.json').then((res) => res.json()),
+  },
+  {
+    name: 'tdesign',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/tdesign/icons.json').then((res) => res.json()),
+  },
+  {
+    name: 'carbon',
+    loader: () =>
+      fetch('https://unpkg.com/@iconify-json/carbon/icons.json').then((res) => res.json()),
+  },
+]);
+
 export const render = async (
   config: MermaidConfig,
   code: string,
